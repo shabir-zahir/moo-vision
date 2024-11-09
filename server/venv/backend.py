ef0b5e2e715ba67b595ec265bfa55e5c6b4faae0
shabir-zahir <shabirzahir1202@gmail.com>
2024-11-08 22:50:30 -0500

    updated model function

--- server/venv/backend.py
+++ server/venv/backend.py
@@ -1,9 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app, origins = '*')
-@app.route("/api/users", methods = ['GET'])
+@app.route("/api/users", methods = ['POST'])
+def get_user_info():
+    if request.method == 'POST':
+
+        data = request.get_json()
+        phone_number = data.get('phone_number')
+        total_cows = data.get('cow_count')
+        youtube_url = data.get('youtube_url')
+        return jsonify({
+            "message": "User created successfully",
+            "phone_number": phone_number,
+            "total_cows": total_cows,
+            "youtube_url": youtube_url
+        })
+
+
 def test():
     return 'test'
 
