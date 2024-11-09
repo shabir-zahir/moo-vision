beb9080b1b83811593f4e8a316d5a69293e27378
shabir-zahir <shabirzahir1202@gmail.com>
2024-11-08 22:55:57 -0500

    moved model file to server folder

---
+++ server/venv/load_file.py
@@ -0,0 +1,45 @@
+import ultralytics
+from ultralytics import YOLO
+import time
+import cv2
+from collections import Counter
+import supervision as sv
+import os
+
+def show_recording(model):
+    source = "https://www.youtube.com/watch?v=QhLMlA3Wb8w&t=19s"  # Example YouTube URL
+    frame_width, frame_height = 640, 480  # Adjust these values to fit your screen
+
+    # Run inference on the source with streaming enabled
+    results = model(source, stream=True, conf = .5)
+
+    # Loop over each frame in the results
+    for result in results:
+        print(len(result), 'NUMBER OF COWS')
+        frame = result.orig_img  # Get the original frame
+
+        # Annotate frame with detection results (bounding boxes, labels, etc.)
+        annotated_frame = result.plot()  # Automatically adds bounding boxes and labels
+
+        # Resize the frame to the desired resolution
+        resized_frame = cv2.resize(annotated_frame, (frame_width, frame_height))
+        # Display the resized frame
+        cv2.imshow("YOLO Real-Time Detection", resized_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+# Release resources
+cv2.destroyAllWindows()
+
+def main():
+
+    model = YOLO('server/venv/moo_vision_v1.pt')
+    
+    #load custom cow detection model
+    show_recording(model)
+    time.sleep(8)
+
+if __name__ == "__main__":
+    main()
