620a5c1a58d614e28cee2f18a09dc8af141c9b6b
shabir-zahir <shabirzahir1202@gmail.com>
2024-11-08 22:31:11 -0500

    pyFlask test commit

---
+++ server/venv/backend.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app, origins = '*')
+@app.route("/api/users", methods = ['GET'])
+def test():
+    return 'test'
+
+if __name__ == "__main__":
+    app.run(debug = True, port = 8080)
